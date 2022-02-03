0fa5bf7ba7dfe2251abe742b238efbd71775f8c4
Marcel Suter <marcel.suter@bzz.ch>
2022-02-03 14:58:17 +0100

    Fixed config & version

--- version.php
+++ version.php
@@ -1,23 +1,39 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * bzz theme config.
+ *
+ * @package   theme_bzz
+ * @copyright 2022 Marcel Suter
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 defined("MOODLE_INTERNAL") || die();
 
-// This is the version of the plugin.
 $plugin->version = 2022020100;
 
-// This is the version of Moodle this plugin requires.
-$plugin->requires = 2016112900.00;  // FixME
+$plugin->requires = 2021051700;
 
-// This is the component name of the plugin - it always starts with 'theme_'
-// for themes and should be the same as the name of the folder.
 $plugin->component = 'theme_bzz';
 
-// This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
-    'theme_lambda' => 2016102100  // FIXME
+    'theme_lambda' => 2020111539
 ];
 
-// This is a stable release.
-$plugin->maturity = MATURITY_ALPHA;  // FIXME
+$plugin->maturity = MATURITY_BETA;
 
 // This is the named version.
 $plugin->release = 0.1;
